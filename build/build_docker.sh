54ce995943445de33120e6b113c950b2bcf780e3
Russel Hunter Yukawa <rhykw@users.noreply.github.com>
2017-09-15 17:15:23 +0900

    Create build_docker.sh

---
+++ build/build_docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+TEST_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%N}}")"; pwd)"
+
+CONTAINER_OS="${CONTAINER_OS:-centos}"
+CONTAINER_TAG="${CONTAINER_TAG:-latest}"
+TEST_CONTAINER="$CONTAINER_OS:ttcopy_test"
+DOCKER_FILE_PATH="$TEST_DIR/Dockerfile"
+cat $DOCKER_FILE_PATH \
+    | sed "s/@@@TAG_NAME@@@/$CONTAINER_TAG/" \
+    | docker build -t $TEST_CONTAINER -
+
+echo "# --------------------"
+echo "# Docker container info"
+echo "# --------------------"
+cat $DOCKER_FILE_PATH \
+    | sed "s/@@@TAG_NAME@@@/$CONTAINER_TAG/"
+echo
+_cmd="docker run -i --rm $TEST_CONTAINER uname -a"
+echo $_cmd
+$_cmd
+
+echo "# --------------------"
+echo "# wget version"
+echo "# --------------------"
+docker run -i --rm $TEST_CONTAINER wget --version
+
+
