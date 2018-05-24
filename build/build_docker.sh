544f501c88d7bd9aa2f163eec6bc5812e4df88f0
rhykw <github@rhykw.net>
2018-05-24 14:01:07 +0900

    fix typo

--- build/build_docker.sh
+++ build/build_docker.sh
@@ -9,40 +9,17 @@ TEST_CONTAINER="${CONTAINER}.builder"
 DOCKER_FILE_PATH="$TEST_DIR/Dockerfile.centos"
 dockerfile=$(mktemp $(pwd)/Dockerfile.XXXXXXXX)
 
-
 mkdir -p rpmbuild/SOURCES
 sources=$(cat *.spec|awk '($1~"^Source"){print $2}')
 if [ ! -z "$sources" ]; then
     ( cd rpmbuild/SOURCES && wget $sources )
 fi
 
-
 cat $DOCKER_FILE_PATH \
     | sed "s/@@@CONTAINER@@@/$CONTAINER/" > $dockerfile
 
 docker build -t $TEST_CONTAINER -f $dockerfile .
 
-# cat $dockerfile | docker build -t $TEST_CONTAINER -f /dev/stdin .
-
-
-echo "# --------------------"
-echo "# Docker container info"
-echo "# --------------------"
-cat $dockerfile
-
-
-#docker image list
-#docker container list -a
-
-
-_cmd="docker run -i --rm $TEST_CONTAINER uname -a"
-echo $_cmd
-$_cmd
-
-echo "# --------------------"
-echo "# wget version"
-echo "# --------------------"
-
 for spec in *.spec;do
-    docker run -i --rm $TEST_CONTAINER rpmbuild --define "__source_yyyy_mm=2018.05" -ba $spec
+    docker run -i --rm $TEST_CONTAINER rpmbuild --define "_source_yyyy_mm 2018.05" -ba $spec
 done
