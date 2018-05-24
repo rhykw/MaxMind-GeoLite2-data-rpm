bd6bba0aae8515362368e04c52894cac6cbed973
rhykw <github@rhykw.net>
2018-05-24 14:13:40 +0900

    auto version

--- build/build_docker.sh
+++ build/build_docker.sh
@@ -15,11 +15,14 @@ if [ ! -z "$sources" ]; then
     ( cd rpmbuild/SOURCES && wget $sources )
 fi
 
+mmdb=$(tar --wildcards -ztf rpmbuild/SOURCES/GeoLite2-Country.tar.gz '*.mmdb')
+source_date=$(IFS='-_/';set -- $mmdb;echo $3)
+
 cat $DOCKER_FILE_PATH \
     | sed "s/@@@CONTAINER@@@/$CONTAINER/" > $dockerfile
 
 docker build -t $TEST_CONTAINER -f $dockerfile .
 
 for spec in *.spec;do
-    docker run -i --rm $TEST_CONTAINER rpmbuild --define "_source_yyyy_mm 2018.05" -ba $spec
+    docker run -i --rm $TEST_CONTAINER rpmbuild --define "_source_yyyy_mm ${source_date:0:4}.${source_date:4:2}" -ba $spec
 done
